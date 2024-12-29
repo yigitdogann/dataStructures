b57a4a3648ed64232c47e8b5a8c270f4bf79a99c
Yigit Dogan <yigitdogan2k@gmail.com>
2024-12-29 14:57:43 +0300

    Update stacks.c

--- stacks.c
+++ stacks.c
@@ -1,3 +1,8 @@
+/*	
+*	STACKS - LIFO
+* Last In First Out
+* Implemented by pointers.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,6 +16,8 @@ stack* sp = NULL;//top item of stack.
 void push(int x);
 void pop();
 void printer();
+void reset();
+stack* topAddress();
 
 int main(void) {
 	printf("push-1\npop-2\nend-3\n->");
@@ -21,27 +28,29 @@ int main(void) {
 		{
 		case 1:
 			printf("enter a number: ");
-			int n = scanf("%d", &n);
+			int n; scanf("%d", &n);
 			push(n);
 			break;
 		case 2: 
 			pop();
 			break;
 		case 3:
-			return 0;
 			break;
 		default: 
 			break;
 		}
-		puts("");
-		printf("push-1\npop-2\nend-3\n->");
-		scanf("%d", &decide);
 
-		printer();
+		printf("\npush-1\npop-2\nend-3\n->");
+		scanf("%d", &decide);
 	}
+
+	printer();
+	reset(); //tested - works fine.
+
 	return 0;
 }
 
+//adds new items to stack. always to the top (or beginning). checks malloc error before adding.
 void push(int x) {
 	stack* p;
 	p = (stack*)malloc(sizeof(stack));
@@ -57,6 +66,7 @@ void push(int x) {
 	}
 }
 
+//deletes the last added item (top). re-arranges the top pointer before deleting
 void pop() {
 	stack* p;
 
@@ -72,9 +82,29 @@ void pop() {
 	}
 }
 
+//prints the stack, visitor is a pointer which traverse until NULL
 void printer() {
-	stack* traverser = sp;
-	while (traverser!=NULL) {
-
+	stack* visitor = sp;
+	printf("\nTOP-->");
+	while (visitor!=NULL) {
+		printf("\t|  %d  |\n", visitor->data);
+		visitor = visitor->next;
 	}
+
+	printf("\nTop item's address = %p\n", topAddress());
+}
+
+//sets the "top pointer" to the next, and delete until NULL
+void reset() {
+	stack* p= NULL;
+	while (sp != NULL) {
+		p = sp;
+		sp = sp->next;
+		free(p);
+	}
+}
+
+//returns the top item's address. Actually the address of the stack
+stack* topAddress() {
+	return sp;
 }
